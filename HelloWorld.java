2d37588dfd58482d8391fe9000da05a2e3dbc8ef
FabioAJ <fabioaugusto.aj@gmail.com>
2018-09-28 16:41:39 -0300

    Calculo da casa

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,9 +1,14 @@
 
-public class HelloWorld {
+class CasaRet {
 
     public static void main(String[] args) {
-        // Prints "Hello, World" to the terminal window.
-        System.out.println("Hello, World");
+      //Fala para usuario
+        System.out.println("Programa para calculo\nda
+        area da casa");
+        System.out.println("A area da sala é "+ 10*10);
+        System.out.println("A area do quarto é " + 5*7);
+        System.out.println("A area do banheiro é" + 5*7);
+        System.out.println("A area total é " + 10*10 + 5*7 + 5*7);
     }
 
 }
