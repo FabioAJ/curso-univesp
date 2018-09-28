935737df30424f5fd4e3b768e45ac86213de2678
FabioAJ <fabioaugusto.aj@gmail.com>
2018-09-28 16:55:53 -0300

    Programa para calculo da casa

---
+++ CasaRet.java
@@ -0,0 +1,12 @@
+public class CasaRet {
+
+    public static void main(String[] args) {
+      //Fala para usuario
+        System.out.println("Programa para cálculo \n da área da casa");
+        System.out.println("A area da sala é " + 10*10);
+        System.out.println("A area do quarto é " + 5*7);
+        System.out.println("A area do banheiro é " + 5*7);
+        System.out.println("A area total é " + 10*10 + 5*7 + 5*7);
+    }
+
+}
