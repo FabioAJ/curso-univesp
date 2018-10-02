9f3d9ddde0bb1deda372633f2ef2502218422eb9
FabioAJ <fabioaugusto.aj@gmail.com>
2018-10-02 16:36:24 -0300

    Aula 6 usando Math, Metodos

--- CasaRet.java
+++ CasaRet.java
@@ -1,29 +1,12 @@
 public class CasaRet {
 
     public static void main(String[] args) {
-      //NAN
-      double x = 0;
-      double y = 0;
-      System.Out.println(x/y);
-      //Variaveis
-      //área do quarto
-      float areaq;
-      //area da sala
-      float areas;
-      //area total
-      float areat;
-      //lateral e quarto
-      float lateral = 10;
-      float cquarto = 7;
-      //float = numeros nao inteiros
-      System.out.println("programa para calculo da area da casa");
-      areas = lateral*lateral;
-      System.out.println("A area da sala e "+areas);
-      areaq = cquarto*(lateral/2);
-      System.out.println("A area do quarto e "+areaq);
-      System.out.println("A area do banheiro e "+areaq);
-      areat = areas +2*areaq;
-      System.out.println("A area taotal e " + areat);
-    }
-
+    // raio da piscina
+    double raio = 2;
+    //area da piscina
+    double areap;
+    //valor do pi
+    areap = Math.PI * Math.pow(raio,2);
+    System.Out.Println("Area: "+areap);
+  }
 }
