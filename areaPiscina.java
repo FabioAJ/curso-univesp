93ebe52e2ba80b065f15e96b581a20d99b8d7040
FabioAJ <fabioaugusto.aj@gmail.com>
2018-10-02 17:39:25 -0300

    Aula 6 Area Piscina

---
+++ areaPiscina.java
@@ -0,0 +1,11 @@
+public class areaPiscina {
+     public static void main(String[] args) {
+// raio da piscina
+    double raio = 2;
+    //area da piscina
+    double areap;
+    //valor do pi
+    areap = Math.PI * Math.pow(raio,2);
+    System.out.println("Area: "+areap);
+  }
+}
