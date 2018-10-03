1bfcd5ef6a526076c644c459b3c14a9d47b6ad03
FabioAJ <fabioaugusto.aj@gmail.com>
2018-10-03 16:33:00 -0300

    Aula 7 Usando Parametros

--- areaCasa.java
+++ areaCasa.java
@@ -1,20 +1,20 @@
-class AreaCasa {
+class areaCasa {
    static void areaCasa(float lateral,float cquarto) {
-
+//Variaveis
   float areaq;
   float areas;
   float areat;
-
-  System.out.println("PROGRAMA PARA CALCULO DE AREA DA CASA");
+//Programacao
+  System.out.println("Programa para calculo da area da casa");
   areas = lateral*lateral;
-  System.out.println("A area da sala é "+areas);
+  System.out.println("A area da sala e "+areas);
   areaq= cquarto*(lateral/2);
-  System.out.println("A area do quarto é "+areaq);
-  System.out.println("A area do banheiro é "+areaq);
+  System.out.println("A area do quarto e "+areaq);
+  System.out.println("A area do banheiro e "+areaq);
   areat = areas+2*areaq;
-  System.out.println("A area total é "+areat);
-  System.out.println("..."+ areat);
+  System.out.println("A area total e "+areat);
 }
+//Metodos de MATH, PI
   static double areaPiscina(double raio){
    return Math.PI*
       Math.pow(raio,2);
@@ -23,9 +23,8 @@ class AreaCasa {
  public static void main(String[] args) {
 
  double areap;
-
  areaCasa(11,	7);
  areap = areaPiscina(2);
- System.out.println("baba" + areap);
- }
+ System.out.println("areaDaPiscina" + areap);
+  }
 }
