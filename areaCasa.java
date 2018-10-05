abb12f55db49429e5620808f9927182a7bd1064d
FabioAJ <fabioaugusto.aj@gmail.com>
2018-10-05 15:28:02 -0300

    Aula 9 Booleans

--- areaCasa.java
+++ areaCasa.java
@@ -2,47 +2,54 @@ class AreaCasa {
   static double valorM2 = 1500.0;
 
   static void areaCasa(double lateral,double cquarto) {
-            //Atencao Parametros = lateral e cquarto sem definicao
-            //Variaveis
-              double areaq;
-              double areas;
-              double areat;
-            //Programacao
-              System.out.println("Programa para calculo da area da casa");
+    //Atencao Parametros = lateral e cquarto sem definicao
+    //Variaveis
+    double areaq;
+    double areas;
+    double areat;
+    //Programacao
+    System.out.println("Programa para calculo da area da casa");
 
-              areas = lateral*lateral;
-              System.out.println("A area da sala e "+areas);
+    areas = lateral*lateral;
+    System.out.println("A area da sala e "+areas);
 
-              areaq= cquarto*(lateral/2);
+    areaq= cquarto*(lateral/2);
 
-              System.out.println("A area do quarto e "+areaq);
+    System.out.println("A area do quarto e "+areaq);
 
-              System.out.println("A area do banheiro e "+areaq);
+    System.out.println("A area do banheiro e "+areaq);
 
-              areat = areas+2*areaq;
+    areat = areas+2*areaq;
 
-              System.out.println("A area total e "+areat);
+    System.out.println("A area total e "+areat);
             }
-//Metodos de MATH, PI
-//raio sem parametro!
-  static double areaPiscina(double raio){
-   return Math.PI*Math.pow(raio,2);
- }
+            //Metodos de MATH, PI
+            //raio sem parametro!
+   static double areaPiscina(double raio){
+     return Math.PI*Math.pow(raio,2);
+   }
 
   static double valor(double area){
-    valorM2 = 5;
-    return(valorM2*area);
+    if (area >= 0){
+      return(valorM2*area);
+    }
+    return(-1);
   }
 
+  public static void main(String[] args) {
 
-public static void main(String[] args) {
+  double areap;
+  areaCasa(11 , 7);
+  areap = areaPiscina(2);
+  System.out.println("A area da piscina e " + areap);
+  //Usando BOOL
+  double preco;
+  boolean valorOK = false;
 
-double areap;
-//Parametros de Area casa definidos!
-areaCasa(11.4,7.2);
-//Parametro de raio!
-areap = areaPiscina(2.0);
-System.out.println("A area da piscina e " + areap);
+  preco = valor(20);
+  valorOK = (preco >= 0);
 
- }
+  if (valorOK) System.out.println("O valor da construcao e "+ preco);
+  else System.out.println("Valor de area negativo");
+  }
 }
