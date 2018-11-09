96e8e75f55b9ef954072b66766936107cd2f65af
FabioAJ <fabioaugusto.aj@gmail.com>
2018-11-09 17:18:33 -0200

    Aula 16 Usando Caracteres

--- areaCasa.java
+++ areaCasa.java
@@ -14,10 +14,16 @@ https://www.youtube.com/playlist?list=PLxI8Can9yAHfK6wdaMUO74lmotAP7J7bi
 
 class AreaCasa {
     static double[] preco = {1500, 1100, 750, 500}; // precos dos materiais
+
     static final int alvenaria = 0;// Material alvenaria
     static final int vinil = 1; //Material vinil
     static final int fibra = 2; //Material fibra
     static final int plastico = 3; //Material plastico
+
+    static char[] nAlvenaria = {'A', 'l', 'v', 'e', 'n', 'a', 'r', 'i', 'a'};
+    static char[] nVinil = {'V', 'i', 'n', 'i', 'l'};
+    static char[] nFibra = {'F', 'i', 'b', 'r', 'a'};
+    static char[] nPlastico = {'P', 'l', 'a', 's', 't', 'i', 'c', 'o'};
 
     static double valorM2 = 1500.0;
 
@@ -27,27 +33,27 @@ class AreaCasa {
         double areas;
         double areat;
 
-    if (!(lateral >= 0 && cquarto >= 0))
-      System.out.println("Erro: Parametro >= 0");
-    else {
-      System.out.println("Programa para calculo da area da casa");
-      areas = lateral*lateral;
-      System.out.println("A area da sala e "+areas);
-      areaq= cquarto*(lateral/2);
-      System.out.println("A area do quarto e "+areaq);
-      System.out.println("A area do banheiro e "+areaq);
-      areat = areas+2*areaq;
-      System.out.println("A area total e "+areat);
+        if (!(lateral >= 0 && cquarto >= 0))
+        System.out.println("Erro: Parametro >= 0");
+        else {
+            System.out.println("Programa para calculo da area da casa");
+            areas = lateral*lateral;
+            System.out.println("A area da sala e "+areas);
+            areaq= cquarto*(lateral/2);
+            System.out.println("A area do quarto e "+areaq);
+            System.out.println("A area do banheiro e "+areaq);
+            areat = areas+2*areaq;
+            System.out.println("A area total e "+areat);
+        }
     }
-  }
 
-  static double valor(double area) {
-    if (area >= 0)
-    return(valorM2*area);
-    return(-1);
-  }
+    static double valor(double area) {
+        if (area >= 0)
+        return(valorM2*area);
+        return(-1);
+    }
 
-  static double valorPiscina(double area, int material) {
+    static double valorPiscina(double area, int material) {
         if (material < alvenaria || material > plastico || area < 0) return (-1);
         return(area * preco[material]);
     }
@@ -59,15 +65,37 @@ class AreaCasa {
         return (resp/arranjo.length);
     }
 
-    public static void main(String[] args) {
-
-        System.out.println("Area\tMaterial\tValor");
-        for (double area = 50; area <= 200; area = area + 50) {
-            for (int tipo = alvenaria; tipo <= plastico; tipo = tipo + 1) {
-                System.out.println(area + "\t" + tipo + "\t\t" + valorPiscina(area,tipo));
-            }
-        }
-        System.out.println(media(preco));
+    //verifica se é minuscula
+    static boolean minuscula(char c) {
+        return(c >= 'a' && c <= 'z');
     }
 
+    public static void main(String[] args) {
+
+        //Percorre a tabela Ascii
+        for (int i = 32; i <= 126; i++) {
+            System.out.println(i+" : "+(char)i);
+        }
+
+        System.out.println("Piscina de ");
+        System.out.println(nFibra);
+        System.out.println(": "+ valorPiscina(100,fibra));
+        //Usando a media dos precos
+        System.out.println(media(preco));
+    }
 }
+// System.out.println("Area\tMaterial\tValor");
+// for (double area = 50; area <= 200; area = area + 50) {
+//     for (int tipo = alvenaria; tipo <= plastico; tipo = tipo + 1) {
+//         System.out.println(area + "\t" + tipo + "\t\t" + valorPiscina(area,tipo));
+//     }
+// } Usado no Main
+
+//Aumentando tabela ASCII
+// char c = 'o';
+// char x = '\u00F6';
+// int y = 246;
+
+// System.out.println(c);
+// System.out.println(x);
+// System.out.println((char)y);
