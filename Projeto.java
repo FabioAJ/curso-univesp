9421a41ec67244fbb52eb830d837f9b2a5ebcb23
FabioAJ <fabioaugusto.aj@gmail.com>
2018-12-04 17:58:56 -0200

    Da acesso ao endereco de casas de um condominio

--- Projeto.java
+++ Projeto.java
@@ -1,14 +1,45 @@
 class Projeto {
 
-    static double area(AreaCasa casa, AreaPiscina piscina) {
-        return(casa.area() + piscina.area());
-    }
+	Residencia[] condominio;
+	int ultimo = - 1; //último alocado
 
-    public static void main (String[] args) {
-        AreaCasa casaPrinc1 = new AreaCasa(10.0, 5.0);//error
-        AreaCasa casaPrinc2 = new AreaCasa(20.0, 5.0);//error
-        AreaPiscina piscina1 = new AreaPiscina(10);
+	boolean adiconaRes(Residencia r) {
+		if (this.ultimo < this.condominio.length - 1) {
+			ultimo++;
+			this.condominio[ultimo] = r;
+			return (true);
+		}
+		return (false);
+	}
 
-        System.out.println(area(casaPrinc1, piscina1));//error
-    }
+	Projeto(int tam) {
+		condominio = new Residencia[tam];
+	}
+
+	public static void main(String[] args) {
+
+		Projeto proj = new Projeto(3);
+
+		AreaCasa c = new AreaCasa(10, 5);
+		AreaPiscina p = new AreaPiscina(5);
+		Residencia r = new Residencia(c, p);
+		proj.adiconaRes(r);
+
+		c = new AreaCasa(12, 7);
+		p = new AreaPiscina(6);
+		r = new Residencia(c, p);
+		proj.adiconaRes(r);
+
+
+		c = new AreaCasa(10, 6);
+		p = new AreaPiscina(3.5);
+		r = new Residencia(c, p);
+		proj.adiconaRes(r);
+
+		for (Residencia re : proj.condominio) {
+			System.out.println("Endereço do objeto: " + re);
+		}
+
+		System.out.println(proj.condominio[1].casa.area());
+	}
 }
---
+++ Residencia.java
@@ -0,0 +1,9 @@
+class Residencia {
+	AreaCasa casa;
+	AreaPiscina piscina;
+
+	Residencia(AreaCasa casa, AreaPiscina piscina) {
+		this.casa = casa;
+		this.piscina = piscina;
+	}
+}
